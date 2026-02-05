5ae4fe67037cede073d31e90555c6039cfa2c9c4
Robert Bradley <rbradaws@amazon.co.uk>
2026-02-05 14:36:49 +0100

    updates

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/hotelapp/',
   server: {
     port: 5173,
     host: '0.0.0.0', // Allow external connections
